d35c0244034aecb79c1703ef43f050f4ed6973ba
James Mills <prologic@shortcircuit.net.au>
2022-09-27 13:27:04 +1000

    WIP: Get this workign on iOS...

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/usr/local/Caskroom/flutter/3.0.2/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/prologic/tmp/flutter_go/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/prologic/tmp/flutter_go/example/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/prologic/tmp/flutter_go/example/.dart_tool/package_config.json"
